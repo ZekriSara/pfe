452f9acb4cb20fb9c2430c640a5d285e1240c066
ahlem <ouafek.ahlem@gmail.com>
2022-04-18 13:31:45 +0100

    ADD MIGRATIONS

--- Projet/User/admin.py
+++ Projet/User/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Norme,Chapitre,SC_niv1,SC_niv2,SC_niv3,Question_Generale,Test,Reponse,Point
 # Register your models here.
+
+admin.site.register(Norme)
+admin.site.register(Chapitre)
+admin.site.register(SC_niv1)
+admin.site.register(SC_niv2)
+admin.site.register(SC_niv3)
+admin.site.register(Point)
+admin.site.register(Question_Generale)
+admin.site.register(Test)
+admin.site.register(Reponse)
+
