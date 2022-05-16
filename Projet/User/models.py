06dc4e49ed445013c66765155a221e46c96a18cb
ahlem <ouafek.ahlem@gmail.com>
2022-05-16 11:12:35 +0100

    update no sens

--- Projet/User/models.py
+++ Projet/User/models.py
@@ -177,7 +177,7 @@ class Question_Generale(models.Model):
 
 
 class Reponse(models.Model):
-<<<<<<< HEAD
+
     id_reponse=models.TextField(primary_key=True)
     id_test=models.ForeignKey(Test,verbose_name="Tests",on_delete=models.CASCADE)
     id_qst=models.ForeignKey(Question_Generale,verbose_name="Questions_Generales",on_delete=models.CASCADE)
@@ -201,19 +201,3 @@ class FileModel(models.Model):
     def __str__(self) -> str:
     
         return str(self.doc)
-=======
-    id_reponse = models.TextField(primary_key=True)
-    id_test = models.ForeignKey(Test, verbose_name="Tests", on_delete=models.CASCADE)
-    id_qst = models.ForeignKey(Question_Generale, verbose_name="Questions_Generales", on_delete=models.CASCADE)
-    id_norme = models.ForeignKey(Norme, verbose_name=("Normes"), on_delete=models.CASCADE)
-    version = models.IntegerField()
-    id_chap = models.ForeignKey(Chapitre, verbose_name="Chapitres", on_delete=models.CASCADE)
-    id_Sc1 = models.ForeignKey(SC_niv1, verbose_name=("Sc_niv1"), on_delete=models.CASCADE, default=None, blank=True,
-                               null=True)
-    id_Sc2 = models.ForeignKey(SC_niv2, verbose_name=("Sc_niv2"), on_delete=models.CASCADE, default=None, blank=True,
-                               null=True)
-    id_Sc3 = models.ForeignKey(SC_niv3, verbose_name=("Sc_niv3"), on_delete=models.CASCADE, default=None, blank=True,
-                               null=True)
-    id_point = models.ForeignKey(Point, verbose_name="Points", on_delete=models.CASCADE)
-    reponse = models.BooleanField()
->>>>>>> ea61880143fa6eb61b709c175b341764128437f7
